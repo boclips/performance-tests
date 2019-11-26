d96e89b7a8f685b589fe60cf8382c0a9dc7ff4c9
Andrew Bruce <me@andrewbruce.net>
2019-11-26 12:55:41 +0000

    Initial CI script that logs in and outputs HTML
    Form factor doesn't seem to work yet. Currently showing mobile-style
    screenshots.
    
    [#169704741]
    
    Signed-off-by: Rebecca Appleyard <rebecca@boclips.com>

---
+++ main.ts
@@ -0,0 +1,81 @@
+import * as puppeteer from 'puppeteer';
+import * as lighthouse from 'lighthouse';
+
+const email: string = process.env.EMAIL;
+const password: string = process.env.PASSWORD;
+const url: string = process.env.URL;
+const port = 8675;
+
+async function login(browser, origin) {
+  const page = await browser.newPage();
+  await page.goto(origin);
+  await page.waitForSelector('input[type="email"]', {visible: true});
+
+  const emailInput = await page.$('input[type="email"]');
+  await emailInput.type(email);
+  const passwordInput = await page.$('input[type="password"]');
+  await passwordInput.type(password);
+  await Promise.all([
+    page.$eval('#kc-form-login', form => form.submit()),
+    page.waitForNavigation(),
+  ]);
+
+  await page.close();
+}
+
+async function logout(browser, origin) {
+  const page = await browser.newPage();
+  await page.goto(`${origin}/logout`);
+  await page.close();
+}
+
+function isMissing(setting: string) {
+  return typeof setting === 'undefined' || setting === '';
+}
+
+async function main() {
+  if ( isMissing(email)
+    || isMissing(password)
+    || isMissing(url)
+  ) {
+    return Promise.reject('Please set EMAIL, PASSWORD and URL');
+  }
+  const browser = await puppeteer.launch({
+    args: [`--remote-debugging-port=${port}`],
+    headless: true,
+    slowMo: 50,
+    defaultViewport: {
+      width: 1200,
+      height: 900
+    }
+  });
+
+  await login(browser, url);
+
+  const config = {
+    extends: 'lighthouse:default',
+    settings: {
+      maxWaitForFcp: 15 * 1000,
+      maxWaitForLoad: 35 * 1000,
+      emulatedFormFactor: 'desktop',
+      throttling: {
+        rttMs: 40,
+        throughputKbps: 10 * 1024,
+        cpuSlowdownMultiplier: 1,
+      }
+    },
+    disableDeviceEmulation: true,
+    output: 'html',
+    port: port
+  };
+
+  const result = await lighthouse(url, config);
+  await browser.close();
+
+  console.log(result.report);
+}
+
+main().catch(err => {
+  console.error(err);
+  process.exit(1);
+});
